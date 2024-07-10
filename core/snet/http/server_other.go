f376c7971c7ac7b456634d7e089ab04e5b8d622f
wwengg <wwwwangg@163.com>
2024-07-10 15:32:59 +0800

    no message

--- core/snet/http/server_other.go
+++ core/snet/http/server_other.go
@@ -8,16 +8,23 @@
 package http
 
 import (
+	"net/http"
 	"time"
 
-	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 )
 
 func InitServer(address string, router *gin.Engine) server {
-	s := endless.NewServer(address, router)
-	s.ReadHeaderTimeout = 20 * time.Second
-	s.WriteTimeout = 20 * time.Second
-	s.MaxHeaderBytes = 1 << 20
-	return s
+	// s := endless.NewServer(address, router)
+	// s.ReadHeaderTimeout = 20 * time.Second
+	// s.WriteTimeout = 20 * time.Second
+	// s.MaxHeaderBytes = 1 << 20
+	// return s
+	return &http.Server{
+		Addr:           address,
+		Handler:        router,
+		ReadTimeout:    20 * time.Second,
+		WriteTimeout:   20 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 }
