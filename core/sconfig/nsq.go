3b030fc1c4be7caed0b465877343354084335efc
wwengg <wwwwangg@163.com>
2024-07-17 00:41:52 +0800

    no message

--- core/sconfig/nsq.go
+++ core/sconfig/nsq.go
@@ -5,7 +5,6 @@
 package sconfig
 
 type Nsq struct {
-	Nsqd1Addr      string `json:"nsqd1Addr" yaml:"nsqd1-Addr" mapstructure:"nsqd1-addr"`
-	Nsqd2Addr      string `json:"nsqd2Addr" yaml:"nsqd2-Addr" mapstructure:"nsqd2-addr"`
-	NsqlookupdAddr string `json:"nsqlookupdAddr" yaml:"nsqlookupd-addr" mapstructure:"nsqlookupd-addr"`
+	NsqlookupdAddr string   `json:"nsqlookupdAddr" yaml:"nsqlookupd-addr" mapstructure:"nsqlookupd-addr"`
+	NsqdAddrList   []string `json:"nsqdAddrList" yaml:"nsqd-addr-list" mapstructure:"nsqd-addr-list"`
 }
