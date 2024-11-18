96353d66985adbe29495af19010f8396262e3bb1
wwengg <wwwwangg@163.com>
2024-11-18 19:37:44 +0800

    Keyformat

--- core/store/hash_map.go
+++ core/store/hash_map.go
@@ -7,34 +7,34 @@ import (
 )
 
 type HashMap struct {
-	keyformat string
+	Keyformat string
 }
 
 func (h *HashMap) GetKeyFormat() string {
-	return h.keyformat
+	return h.Keyformat
 }
 
 func (h *HashMap) SetKeyFormat(format string) {
-	h.keyformat = format
+	h.Keyformat = format
 }
 
 func (h *HashMap) Delete(id int64) error {
 	if RedisIns() == nil {
 		return errors.New("redis is nil")
 	}
-	return RedisIns().RedisCli.HDel(context.Background(), h.keyformat, fmt.Sprintf("%d", id)).Err()
+	return RedisIns().RedisCli.HDel(context.Background(), h.Keyformat, fmt.Sprintf("%d", id)).Err()
 }
 
 func (h *HashMap) HSet(id int64, data interface{}) error {
 	if RedisIns() == nil {
 		return errors.New("redis is nil")
 	}
-	return RedisIns().RedisCli.HSet(context.Background(), h.keyformat, fmt.Sprintf("%d", id), data).Err()
+	return RedisIns().RedisCli.HSet(context.Background(), h.Keyformat, fmt.Sprintf("%d", id), data).Err()
 }
 
 func (h *HashMap) HGet(id int64) (string, error) {
 	if RedisIns() == nil {
 		return "", errors.New("redis is nil")
 	}
-	return RedisIns().RedisCli.HGet(context.Background(), h.keyformat, fmt.Sprintf("%d", id)).Result()
+	return RedisIns().RedisCli.HGet(context.Background(), h.Keyformat, fmt.Sprintf("%d", id)).Result()
 }
