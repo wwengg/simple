07e3ac6c2605f00f3b9f51b3a96dac4cc7ce878f
wwengg <wwwwangg@163.com>
2024-11-24 23:20:39 +0800

    no message

--- core/utils/compressor_win.go
+++ core/utils/compressor_win.go
@@ -3,16 +3,12 @@
 
 package utils
 
-import "github.com/wwengg/simple/core/slog"
-
 type BrotliCompressor struct{}
 
 func (c BrotliCompressor) Zip(data []byte) ([]byte, error) {
-	slog.Ins().Warnf("Windows Zip")
 	return data
 }
 
 func (c BrotliCompressor) Unzip(data []byte) ([]byte, error) {
-	slog.Ins().Warnf("Windows Unzip")
 	return data
 }
